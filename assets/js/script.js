073756117bfd02772ad22af99667408243708c22
Prima Aulia Gusta <primaulia@gmail.com>
2017-03-27 13:17:29 +0800

    update for gh pages

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,7 +3,7 @@ $(function () {
 
   function Reeler (prop) {
     this.prop = prop
-    this.tunefind_url = 'https://crossorigin.me/https://www.tunefind.com/api/v1/movie'
+    this.tunefind_url = 'https://www.tunefind.com/api/v1/movie'
     this.username = 'ae74be736f4a59ab6b25b012248a1ef9'
     this.password = '699f38f8f30c9a5a6e3c58e5b738ad32'
   }
